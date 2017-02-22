0e37792862b4b447fcba6324435fd6888c224f10
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2017-02-22 20:35:26 +0100

    cmd/ipfs-objects: WIP

--- src/cmd/ipfs-objects/main.go
+++ src/cmd/ipfs-objects/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
+
 	ipfs_objects "ipfs-objects"
 	"ipobj"
+
+	ic "gx/ipfs/QmfWDLQjGjVe4fr5CoztYW2DYYjRysMJrFe1RCsXLPTf46/go-libp2p-crypto"
 )
 
 func Main() {
 	flag.Parse()
 
-	var net ipobj.Network = ipfs_objects.NewNetwork()
+	var err error
+	var config ipfs_object.NetworkConfig
+	var net ipobj.Network
+
+	net, err = ipfs_objects.NewNetwork(context.Background(), config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	_ = net
 }
